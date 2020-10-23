c43e3d1ba2e17e6eff001c2630abf935cb58cb27
Bobbie Archambault <bmarchambault@gmail.com>
2020-10-22 22:58:45 -0500

    learning about the state property 1

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-
+    //add the state property and set it to an object.
+    //state is a special property in react components,  its an object that includes data that the component needs.
+    state = {
+        count: 0
+    };
     render() {
+
         return (
-            <div>
-                <h1>Hello Bobbie</h1>
+            // <div>  instead of another div (because the root div is a div already, use fragment
+            <React.Fragment>
+                {/*//instead of hardcoding below, use the state method to add values dynamically*/}
+              {/*<h1>Hello Bobbie</h1>*/}
+              {/*//change the above to span once the state property is implemented.*/}
+                <span>{this.state.count}</span>
                 <button>Increment</button>
-            </div>
+            </React.Fragment>
+
+            // </div>
         );
     }
 }
