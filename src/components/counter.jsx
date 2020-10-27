720921afa89614316bed0ef53d81730f39c36c6c
Bobbie Archambault <bmarchambault@gmail.com>
2020-10-27 15:23:23 -0500

    learned embedding expressions and JSX

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,19 +7,27 @@ class Counter extends Component {
         count: 0
     };
     render() {
-
         return (
-            // <div>  instead of another div (because the root div is a div already, use fragment
+            // <div>  --instead of another div (because the root div is a div already, use fragment
             <React.Fragment>
-                {/*//instead of hardcoding below, use the state method to add values dynamically*/}
+                {/*--instead of hardcoding below, use the state method to add values dynamically*/}
               {/*<h1>Hello Bobbie</h1>*/}
-              {/*//change the above to span once the state property is implemented.*/}
-                <span>{this.state.count}</span>
+              {/*-- change the above to span once the state property is implemented.*/}
+              {/*  <span>{this.state.count}</span>*/}
+                {/*--in between the curly braces, we can write any valid javascript expression.  even call a function.*/}
+                <span>{this.formatCount()}</span>
                 <button>Increment</button>
             </React.Fragment>
-
             // </div>
         );
+    }
+    formatCount() {
+        // return this.state.count === 0 ? 'Zero' : this.state.count;
+    //   -- when ever there is repetitive code like this.state.count - use object destructuring instead:
+        const {count} = this.state;
+        return count === 0 ? "Zero" : count;
+        //--further more, we can return a jsx expression instead of plain text.
+        // return count === 0 ? <h1> Zero </h1> : count;
     }
 }
 export default Counter;
