757be28ffed318b166ca970bfd63f8f86f27ab09
Bobbie Archambault <bmarchambault@gmail.com>
2020-10-30 12:42:02 -0500

    learning how to render classes dynamically

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,31 +2,32 @@ import React, { Component } from "react";
 
 class Counter extends Component {
     state = {
-        count: 0,
-        //    random 200 X 200 pic:
-        //     imageURL: 'https://picsum.photos/200'
+        count: 0
     };
-    //  -- can apply styles this way:
-    //  styles = {
-    //      fontSize: 15,
-    //      fontWeight: "bold"
-    //  }  ;
 
     render() {
+        //rather than polluting the render method with below.  turn it into a method.  right click, refactor, extract method, choose location and rename:
+        // let classes = "badge m-2";
+        // //if count = 0, use the yellow badge.  else the blue:
+        // classes += (this.state.count === 0) ? "badge-warning" : "badge-primary"
         return (
             <React.Fragment>
-{/*-----we want the image src to be dynamic.  we can hard code the source, or: */}
-                {/*<img src={this.state.imageURL} alt=""/>*/}
-
-{/*------ we add classes buy using the attribute className=""  */}
-                <span className=" badge badge-primary m-2 ">{this.formatCount()}</span>
-{/*-----------STYLING-----------*/}
-            {/*<span style={this.styles} className=" badge badge-primary m-2 ">{this.formatCount()}</span>*/}
-{/*------- inline styles no need for style property above and use double curly braces:   */}
-            {/*    <span style={{fontSize: 30}} className=" badge badge-primary m-2 ">{this.formatCount()}</span>*/}
+                {/*replace the hard coded classname with classes: */}
+                {/*<span className=" badge badge-primary m-2 ">{this.formatCount()}</span>*/}
+                {/*<span className=" classes ">{this.formatCount()}</span>*/}
+                {/*--after extraction of the method, we can now call the method :*/}
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button className="btn btn-secondary btn-sm">Increment</button>
             </React.Fragment>
         );
+    }
+    //the extracted method below:
+    getBadgeClasses() {
+        let classes = "badge m-2";
+        //if count = 0, use the yellow badge.  else the blue:
+        classes += this.state.count === 0 ? "badge-warning" : "badge-primary"
+        //needed to add a return here, unsure why it didn't do it automatically.
+        return classes;
     }
     formatCount() {
         const {count} = this.state;
