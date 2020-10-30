18a04d30b1ca37df6a6484eea182b94eede1ef3a
Bobbie Archambault <bmarchambault@gmail.com>
2020-10-30 14:40:52 -0500

    one way to conditionally rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,20 +4,26 @@ class Counter extends Component {
     state = {
         count: 0,
         tags: ['tag1', 'tag2', 'tag3' ]
+        // tags: []
     };
+    //--Need to use javascript for conditional logic as JSX is not a templating engine.
+    //--create a helper method in JS:
+    renderTags(){
+        if (this.state.tags.length === 0) return <p> There are no tags!</p>;
+        return <ul>{this.state.tags.map(tag => <li key={tag}>{ tag }</li>)}</ul>;
+    // --to test, un comment out the empty tag array.
+    }
 
     render() {
         return (
             <div>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button className="btn btn-secondary btn-sm">Increment</button>
-                <ul>
-                    {/*--use the map method: */}
-                    {/*{this.state.tags.map(tag => <li>{ tag }</li>)}*/}
+                {/*--paste this in to the method.*/}
+                {/*<ul>{this.state.tags.map(tag => <li key={tag}>{ tag }</li>)}</ul>*/}
 
-                    {/*--you will have an error in the console because it needs a key to uniquely identify each item in the list. because if the state of an element in the virtual DOM changes (say, item 3), react need to quickly figure out what element has changed and where in the dom it need to make changes to keep the DOM in sync with the virtual DOM.  So when you use the map method for a list of items set the key in the real world you'll use the property id ex:  key.id:   */}
-                    {this.state.tags.map(tag => <li key={tag}>{ tag }</li>)}
-                </ul>
+            {/* --- now call the helper method: */}
+                { this.renderTags() }
             </div>
         );
     }
